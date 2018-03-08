869a1fafdb934201c8453741b65437f105a9c6ac
seshutodla <szt0046@tigermail.auburn.edu>
2018-03-07 21:36:13 -0600

    Add files via upload

--- Server/DeviceWifi.js
+++ Server/DeviceWifi.js
@@ -1,34 +1,37 @@
 var wifi = require('node-wifi');
- 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'seshu149',
+	database: 'DeviceInfo'
+});
+connection.connect();
+
 
 wifi.init({
     iface : null 
 });
  
-// Scan networks
-wifi.scan(function(err, networks) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(networks);
-        
-    }
-});
  
-
- 
-
 wifi.getCurrentConnections(function(err, currentConnections) {
     if (err) {
         console.log(err);
     }
+	console.log("List of current connections: ")
     console.log(currentConnections);
-    
+    var id = '34:8f:27:03:7f:7c';
+	connection.query('select * from devices where DeviceID like ?',id, function(err, result){
+	console.log(result);
+	if(result){
+		console.log("This is an IOT device");
+	} else {
+		console.log("This is not an IOT device");
+		}
+	});
+	connection.query('select * from devices', function(err, result){
+		console.log(result);
+	})
 });
  
-
-wifi.scan().then(function (networks) {
-  // networks
-}).catch(function (error) {
-  // error
-})
