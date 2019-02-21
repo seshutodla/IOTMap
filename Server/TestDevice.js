d015697a231e081324fe26d170230a7ff1987688
seshutodla <szt0046@tigermail.auburn.edu>
2019-02-21 06:28:16 -0600

    Update TestDevice.js

--- Server/TestDevice.js
+++ Server/TestDevice.js
@@ -1,52 +1,47 @@
-const netList = require('network-list');
+var netList = require('network-list');
 var mysql = require('mysql');
-var http = require('http');
 var fs = require('fs');
+var http = require('http');
 
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: 'seshu149',
-	database: 'DeviceInfo'
+	database: 'deviceinfo'
 });
-connection.connect();
 
-
-netList.scanEach({ip: '172.20.10'}, (err, obj) => {
-    //console.log(obj.mac); // device object
-    	if(obj.mac == 'b8:27:eb:64:84:3c'){
-    		if(obj.mac){
-    			http.createServer(outLay).listen(8000);
-    		}
-    	else{
-    		http.createServer(basicLay).listen(8000);
+netList.scanEach({}, (err, obj) => {
+	if(obj.alive == true)
+	{
+		if(obj.mac)
+		{
+	    	connection.query('select * from devices where DeviceID like ?', obj.mac, function(err,result){
+	    		console.log(result);
+		    	if(result.length > 0){
+		    		console.log('Is IOT');
+		    		console.log(obj);
+		    		console.log('----')
+		    	}
+		    	else{
+		    		console.log('Not IOT');
+		    		console.log(obj);
+		    		console.log('---')
+		    	}
+	    	})
     	}
-    }
-  })
+	}
+});
+http.createServer(outLay).listen(8000);
 
-
- function outLay(request, response) {
- 	response.writeHead(200, {'Content-Type': 'text/html'});
- 	fs.readFile('./outLay.html', null, function(error, data){
- 		if(error) {
- 			response.writeHead(404);
- 			response.write('File not found!');
- 		} else {
- 			response.write(data);
- 		}
- 		response.end();
- 	});
- }
-function basicLay(request, response) {
- 	response.writeHead(200, {'Content-Type': 'text/html'});
- 	fs.readFile('./outIay.html', null, function(error, data){
- 		if(error) {
- 			response.writeHead(404);
- 			response.write('File not found!');
- 		} else {
- 			response.write(data);
- 		}
- 		response.end();
- 	});
- }
-
+function outLay(request, response){
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	fs.readFile('./layoutTest.html', null, function(error, data){
+		if(error){
+			response.writeHead(404);
+			response.write('File not found!');
+		} else {
+			response.write(data);
+		}
+		response.end();
+	});
+}
